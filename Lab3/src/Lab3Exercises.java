148c76a45793768b013318faf11f53ccfcf7d43d
4010b6 <4010b6@melon.ece.uprm.edu>
2017-11-15 18:59:44 -0400

    Frame with background and some borders

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,19 @@
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
+	
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Brian E. Munoz");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 450);
 		myFrame.setSize(200, 200);
+		
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
+		
 		myFrame.setVisible(true);
 	}
+	
 }
+
+	
