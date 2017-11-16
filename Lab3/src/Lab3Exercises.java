e2711530876b1251325caf1064f9bf79917bca07
BrianMunoz <brian.munoz1@upr.edu>
2017-11-15 21:02:28 -0400

    Some errors fixed and polygons added

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,19 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	
 	public static void main(String[] args) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		int width = (int) screenSize.getWidth();
+		int height = (int) screenSize.getHeight();
+		
 		JFrame myFrame = new JFrame("Brian E. Munoz");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 450);
+		myFrame.setLocation(((width/2) - 100), (height/2) - 100);
 		myFrame.setSize(200, 200);
 		
 		MyPanelClass myPanel = new MyPanelClass();
